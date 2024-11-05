e70d57a32003c07e26a22b9f8f769c9864e4f7ea
Stanislav Borisychev <s.borisychev>
2024-11-05 10:43:40 +0100

    C4

--- smart-home-monolith/src/main/resources/init.sql
+++ smart-home-monolith/src/main/resources/init.sql
@@ -1,12 +1,17 @@
-CREATE TABLE IF NOT EXISTS heating_systems (
-    id BIGSERIAL PRIMARY KEY,
-    is_on BOOLEAN NOT NULL,
-    target_temperature DOUBLE PRECISION NOT NULL,
+CREATE TABLE IF NOT EXISTS heating_systems
+(
+    id                  BIGSERIAL PRIMARY KEY,
+    is_on               BOOLEAN NOT NULL,
+    target_temperature  DOUBLE PRECISION NOT NULL,
     current_temperature DOUBLE PRECISION NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS temperature_sensors (
-    id BIGSERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS temperature_sensors
+(
+    id                  BIGSERIAL PRIMARY KEY,
     current_temperature DOUBLE PRECISION NOT NULL,
-    last_updated TIMESTAMP NOT NULL
+    last_updated        TIMESTAMP NOT NULL
 );
+
+INSERT INTO heating_systems(id, is_on, target_temperature, current_temperature)
+VALUES (1, false, 20, 21);
